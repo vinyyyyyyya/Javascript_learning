e624d9be00d544c1db541e91d47db6006f3c33f8
vinayak teradali <vteradali5@gmail.com>
2023-12-25 21:54:17 +0530

    dom 1 done

--- Dom.js
+++ Dom.js
@@ -1,3 +1,8 @@
-// Document object model
-console.log("hello");
-window.console.log(window);
+// Document object mode
+document.getElementById('header2').append(" "+"from Apna College")
+let divs=document.querySelectorAll('.box');
+let i=1;
+for (const div of divs) {
+  div.innerText=`hi ${i}`
+  i++;
+}
