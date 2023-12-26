450169072018530de821d51ebe3771165a1a92eb
vinayak teradali <vteradali5@gmail.com>
2023-12-26 12:49:29 +0530

    new changes

--- Dom.js
+++ Dom.js
@@ -1,8 +1,8 @@
 // Document object mode
-document.getElementById('header2').append(" "+"from Apna College")
-let divs=document.querySelectorAll('.box');
-let i=1;
-for (const div of divs) {
-  div.innerText=`hi ${i}`
-  i++;
-}
+let el = document.createElement("button");
+el.innerText = "click me!";
+el.style.backgroundColor = "red";
+el.style.color = "white";
+document.body.prepend(el);
+let para = document.querySelector("p");
+para.classList.add("newclass");
